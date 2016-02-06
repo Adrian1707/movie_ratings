b82bc5310cc89036a6eca93e41d4dd6dfccb6a62
Adrian Booth <Adrian.booth17@gmail.com>
2016-02-06 15:05:40 +0000

    Film names now found from each cinema

--- cinema_data.rb
+++ cinema_data.rb
@@ -1,17 +1,40 @@
 require 'net/http'
 require 'json'
 
-def parse_data(url)
-url = url
-uri = URI(url)
-response = Net::HTTP.get(uri)
-@cinema_names = JSON.parse(response)
+def get_uri(url)
+  @url = url
+  @uri = URI(url)
+  @response = Net::HTTP.get(@uri)
+end
+
+def parse_cinema_data(url)
+  get_uri(url)
+  @cinema_names = JSON.parse(@response)
+end
+
+def parse_film_data(url)
+  get_uri(url)
+  @film_names = JSON.parse(@response)
 end
 
 def store_names_in_hash
   @cinema_names_ids = []
   @cinema_names["cinemas"].each do |x|
     @cinema_names_ids << {x["id"] => x["name"]}
+  end
+end
+
+def store_films_in_hash
+  @films = []
+  @film_names_ids = []
+  @film_names["films"].each do |x|
+    @film_names_ids << {x["edi"] => x["title"]}
+  end
+end
+
+def collect_film_names
+  @film_names_ids.each do |x|
+    @films << x.values
   end
 end
 
@@ -30,13 +53,24 @@ def fetch_cinema_keys
   @results.each do |x|
     @cinema_keys << x.keys.join('').to_i
   end
-  @cinema_keys
+  @key = @cinema_keys.join('')
 end
 
-parse_data("http://www2.cineworld.co.uk/api/quickbook/cinemas?key=n7Dhu:mz&date=20160211")
+def set_date(date)
+  @date = date
+end
+
+set_date("20160207")
+parse_cinema_data("http://www2.cineworld.co.uk/api/quickbook/cinemas?key=n7Dhu:mz&date=#{@date}")
 store_names_in_hash
-fetch_cinema("Liverpool")
-print fetch_cinema_keys
+fetch_cinema("London - Wembley")
+fetch_cinema_keys
+parse_film_data("http://www2.cineworld.co.uk/api/quickbook/films?key=n7Dhu:mz&cinema=#{@key}&date=#{@date}")
+store_films_in_hash
+collect_film_names
+print @films
+
+
 
 
 current_date = Time.now.strftime("%Y%d%m")
